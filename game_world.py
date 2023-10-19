2edd9ffac751e29d6a0ef96d1f5a259e47c3461c
SU-Lee53 <dltmddnr05@naver.com>
2023-10-19 10:08:52 +0900

    game loop에서 game_world 사용

--- game_world.py
+++ game_world.py
@@ -4,7 +4,15 @@
 objects = []
 
 # 월드에 객체를 넣는 함수
-def world(o):
-	objects.append[o]
+def add(o):
+	objects.append(o)
 
+# 월드를 업데이트하는, 객체들을 모두 업데이트함
+def update():
+	for o in objects:
+		o.update()
 
+# 월드 객체들을 모두 그리기
+def render():
+	for o in objects:
+		o.draw()
