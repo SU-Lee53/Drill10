cf93a7b2563caa72d260706706d04ab4bb2f0931
SU-Lee53 <dltmddnr05@naver.com>
2023-10-19 10:21:15 +0900

    공발사 가능

--- game_world.py
+++ game_world.py
@@ -4,7 +4,7 @@
 objects = []
 
 # 월드에 객체를 넣는 함수
-def add(o):
+def add_object(o):
 	objects.append(o)
 
 # 월드를 업데이트하는, 객체들을 모두 업데이트함
@@ -16,3 +16,7 @@ def update():
 def render():
 	for o in objects:
 		o.draw()
+
+## 객체 삭제
+def remove_object(o):
+	objects.remove(o)
